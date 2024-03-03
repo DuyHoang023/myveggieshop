7682aff31bfe0a8615093cf5074957515b941771
DuyH <tymweaver3@gmail.com>
2024-03-03 21:50:12 +0700

    phpmailer

--- service/EmailService.php
+++ service/EmailService.php
@@ -13,7 +13,7 @@ class EmailService
 
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = SMTP_HOST;                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
--- site/controller/ContactController.php
+++ site/controller/ContactController.php
@@ -12,5 +12,11 @@ class ContactController
     // gởi mail đến chủ cửa hàng
     function sendEmail()
     {
+        $to = SHOP_OWNER;
+        $subject = "Chicken";
+        $content = "Cow";
+        $emailService = new EmailService;
+        $emailService->send($to, $subject, $content);
+        echo 'Đã gởi mail thành công';
     }
 }
