bffcb9c172d2b689b675bb124c3bd71a19e6d2b7
DuyH <tymweaver3@gmail.com>
2024-03-03 21:23:17 +0700

    php mailer

--- config.php
+++ config.php
@@ -3,3 +3,9 @@ define('SEVERNAME', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
 define('DBNAME', 'myvegieshop');
+
+// SMTP (send mail)
+define("SMTP_USERNAME", "tymweaver3@gmail.com");
+define("SMTP_SECRET", "mopyvzjfepwczwxf");
+define("SMTP_HOST", "smtp.gmail.com");
+define("SHOP_OWNER", "tymweaver3@gmail.com");
--- site/controller/ContactController.php
+++ site/controller/ContactController.php
@@ -1,9 +1,5 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
 class ContactController
 {
     // hiển thị trang liên hệ
@@ -16,33 +12,5 @@ class ContactController
     // gởi mail đến chủ cửa hàng
     function sendEmail()
     {
-        //Create an instance; passing `true` enables exceptions
-        $mail = new PHPMailer(true);
-
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'tymweaver3@gmail.com';                     //SMTP username
-            $mail->Password   = 'mopyvzjfepwczwxf';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-            //Recipients
-            $mail->setFrom('tymweaver3@gmail.com');
-            $mail->addAddress('tymweaver@gmail.com');     //Add a recipient
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Myveggieshop - Liên Hệ';
-            $mail->Body    = 'Con gà';
-
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
     }
 }
