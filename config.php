b70beaa5b0dbed7f028cdcd873bd41a630336f40
DuyH <tymweaver3@gmail.com>
2024-03-28 02:05:34 +0700

    #

--- config.php
+++ config.php
@@ -11,5 +11,5 @@ define("SMTP_SECRET", "mopyvzjfepwczwxf");
 define("SMTP_HOST", "smtp.gmail.com");
 define("SHOP_OWNER", "tymweaver3@gmail.com");
 
-define("GOOGLE_RECAPTCHA_SITE", "6LcIjKYpAAAAAIrRldhbypge8cecuVeiWpACn3Hl");
-define("GOOGLE_RECAPTCHA_SECRET", "6LcIjKYpAAAAADu7eXh2MaIJRXOCzp44OL9I1P7w");
+define("GOOGLE_RECAPTCHA_SITE", "6Lcq2qYpAAAAAJLCyf3pcoG_QIE3knmlnyAxoX_E");
+define("GOOGLE_RECAPTCHA_SECRET", "6Lcq2qYpAAAAAPoHH3AqYBvbvDu39hNEOtUqLIdY");
